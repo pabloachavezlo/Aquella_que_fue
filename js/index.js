6aa2b93af5556b44df66095b16a57f4e841be350
Pablox <palchavez@scl.edu.gt>
2024-05-16 16:11:58 -0600

    index contraseñas y error acabado

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 var passwordActual = ['sirena','SIRENA'];
 var indication2 = $('.indicación2');
 
-indication2.on('click', '#confirmar1', function(e) {
+var alerts = [
+    'Esto no es para cualquiera, no te preocupes, intentalo de nuevo! 😘',
+    'Intentalo otra vez, no te rindas 💪',
+    'Vamos... Dale, sé que lo sabes 🙃🙃',
+    'Piensa un poquito más... 🫠',
+    'Otra vez? Enserio...? 🤨',
+    'Ni como ayudarte... 😕',
+    'Está bien... Aunque sea una última vez piensalo... 🫡',
+    'Nada enserio? 🫥',
+    'Está bien, la contraseña es... sirena 🧜‍♀️'
+];
+
+var currentIndex = 0;
+
+document.getElementById('confirmar1').addEventListener('click', function(e) {
     e.preventDefault();
     var pass = indication2.find('#pass1').val();
     if(passwordActual.includes(pass)) {
@@ -12,6 +26,10 @@ indication2.on('click', '#confirmar1', function(e) {
         });
     } else {
         indication2.find('.resultado').html('<p class="incorrecto">Incorrect</p>');
-        alert('Password is incorrect!');
+        alert(alerts[currentIndex]);
+        currentIndex = (currentIndex + 1) % alerts.length;
     }
+
 });
+
+document.getElementById('confirmar1').addEventListener('click', indication2);
