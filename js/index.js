879338a165dff0fcfef6bada380e5b0efbdc9729
Pablox <palchavez@scl.edu.gt>
2024-05-16 13:22:27 -0600

    Cambio de imagen indx

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-var passwordActual = 'sirena';
+var passwordActual = ['sirena','SIRENA'];
 var indication2 = $('.indicación2');
 
 indication2.on('click', '#confirmar1', function(e) {
     e.preventDefault();
     var pass = indication2.find('#pass1').val();
-    if(pass === passwordActual) {
+    if(passwordActual.includes(pass)) {
         indication2.find('.resultado').html('<p class="correcto">Correct</p>');
         $('body').addClass('página-transición');
         $('body').one('transitionend', function() {
